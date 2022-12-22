07f8be859b841aadda8702761a904bc0231c4ca5
Ovikx <ovikdas10@gmail.com>
2022-12-22 12:53:55 -0500

    cache members on startup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -9,8 +9,8 @@ const commandCreator = new CommandCreator();
 export default bot.on('ready', async () => {
     console.log('Bot has logged in!');
     commandCreator.createCommands(GLOBAL);
-    //console.log('Caching members...')
-    //const supportGuild = bot.guilds.add(await bot.getRESTGuild(important.supportGuildId));
-    //console.log(`Cached ${await supportGuild.fetchAllMembers()} members from ${supportGuild.name}`);
+    console.log('Caching members...')
+    const supportGuild = bot.guilds.add(await bot.getRESTGuild(important.supportGuildId));
+    console.log(`Cached ${await supportGuild.fetchAllMembers()} members from ${supportGuild.name}`);
     console.log('Loaded completely!');
 });

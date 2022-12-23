192d0aa60d8abc95ecf00322c37bdbf2d6308dec
Ovikx <ovikdas10@gmail.com>
2022-12-23 15:36:12 -0500

    fix comment typo

--- src/commands/assignRole.ts
+++ src/commands/assignRole.ts
@@ -37,7 +37,7 @@ async function command(bot: Eris.Client, interaction: Eris.CommandInteraction) {
 
         const memberId: string = (interaction.data.options?.[0] as any).value;
 
-        // Interaction must be called from a serer
+        // Interaction must be called from a server
         const guildId = interaction.guildID;
         if (!guildId) {
             throw 'Server invoke only';
